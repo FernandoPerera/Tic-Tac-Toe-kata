ea252d5168317e8a08306a26979da7f0dc8c40cb
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2022-12-09 13:26:20 +0000

    chore: add mockito

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,8 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-    testImplementation("org.assertj:assertj-core:2.9.1")
+    testImplementation("org.assertj:assertj-core:3.20.0")
+    testImplementation("org.mockito:mockito-core:4.7.0")
 }
 
 testing {
