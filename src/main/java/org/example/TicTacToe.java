f0a8d143f7a56ea7b24aa624fd0df0d071fc7fe9
fernandoperera <fernando02perera@gmail.com>
2024-10-30 18:58:51 +0000

    :recycle: added conditional variables

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 public class TicTacToe implements GameBoard {
 
+    private static final String HORIZONTAL_BOARD_DIVISION = "----------";
+    private static final String VERTICAL_BOARD_DIVISION = " | ";
+
     public TicTacToe(){}
     private List<String> board = List.of("1", "2", "3", "4", "5", "6", "7", "8", "9");
 
@@ -17,12 +20,15 @@ public class TicTacToe implements GameBoard {
 
         StringBuilder representation = new StringBuilder();
         for (int i = 0; i < board.size(); i++){
-            if (i == board.size() - 1){
+            boolean isLastRow = i == board.size() - 1;
+            boolean mustPlaceHorizontalDivision = (i + 1) % 3 == 0;
+
+            if (isLastRow){
                 representation.append(board.get(i));
-            }else if ((i + 1) % 3 == 0) {
-                representation.append(board.get(i)).append("\n----------\n");
+            }else if (mustPlaceHorizontalDivision) {
+                    representation.append(board.get(i)).append("\n" + HORIZONTAL_BOARD_DIVISION + "\n");
             } else {
-                representation.append(board.get(i)).append(" | ");
+                    representation.append(board.get(i)).append(VERTICAL_BOARD_DIVISION);
             }
         }
 
