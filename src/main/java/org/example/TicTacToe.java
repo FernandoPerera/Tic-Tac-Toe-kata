87d0b5af3ceeef52c6c3558f0268067e4806a745
fernandoperera <fernando02perera@gmail.com>
2024-11-04 22:50:14 +0000

    :recycle: removed Board logic from TicTacToe

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -1,23 +1,18 @@
 package org.example;
 
 import java.util.Arrays;
-import java.util.List;
 
 public class TicTacToe implements GameBoard {
 
-    private List<String> board = Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9");
+    private final Board BOARD = new Board(Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9"));
 
     @Override
     public void playIn(int position) {
-        boolean isInBoardLimits = position <= 0 || position > board.size();
-        if (isInBoardLimits) {
-            return;
-        }
-        board.set(position - 1, "X");
+        BOARD.occupyCell(position);
     }
 
     @Override
     public String getBoard() {
-        return Printer.display(board);
+        return Printer.display(BOARD.course());
     }
 }
