b819eb63ae61964618c3fdf68c686673e27e812a
fernandoperera <fernando02perera@gmail.com>
2024-11-04 23:05:33 +0000

    :sparkles: created abstract class game

---
+++ src/main/java/org/example/Game.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public abstract class Game {
+
+    protected final Board BOARD;
+
+    public Game(Board BOARD) {
+        this.BOARD = BOARD;
+    }
+
+    protected abstract void playIn(int position);
+
+    public String getBoard() {
+        return Printer.display(BOARD.course());
+    }
+}
