993a8cac1c9ca7538fae4251ab7bea3ea17afa28
FernandoPerera <fernando02perera@gmail.com>
2024-11-13 14:36:58 +0000

    :white_check_mark:  should_win_player_one_if_its_pieces_occupy_a_column

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class TicTacToe extends Game {
@@ -28,7 +29,31 @@ public class TicTacToe extends Game {
 
     @Override
     protected String getStatus() {
+        List<String> boardCells = super.BOARD.getCells();
+
+        for (int i = 0; i < boardCells.size(); i++) {
+
+            String cellValue = boardCells.get(i);
+            if (isGamePiece(cellValue)) {
+
+                boolean isFirstRow = i < 3;
+                if (isFirstRow) {
+                    String centerColValue = boardCells.get(i + 3);
+                    String downColValue = boardCells.get(i + 6);
+
+                    if (cellValue.equals(centerColValue) && cellValue.equals(downColValue))
+                        return GameStates.P1_WINS.name();
+                }
+            }
+        }
+
         return GameStates.PLAYING.name();
+    }
+
+    private boolean isGamePiece(String value) {
+        return Arrays.stream(Piece.values()).anyMatch(
+                piece -> piece.name().equals(value)
+        );
     }
 
 }
